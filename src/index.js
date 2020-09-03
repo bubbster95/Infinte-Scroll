794854329ffabaf663ef8daf4a8dfdd9bdb39654
bubbster95 <stiles.billy@gmail.com>
2020-09-03 19:38:56 -0400

    Fixed Css by adding flexbox

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Nav from './Navigation/index.js';
 import Shop from './Pages/shop.js';
 
 class Site extends React.Component {
     render() {
         return (
             <div className="site">
-                <Nav/>
-                <Shop/>
+                    <Shop/>
             </div>
         );
     }
